d1f96c246c5e1056fa7ae9b865eba143d8c1bd20
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-04-10 22:37:26 +0530

    explanatioin for module

--- 2.Node-Package-manager/4.loadLogger.js
+++ 2.Node-Package-manager/4.loadLogger.js
@@ -1,4 +1,6 @@
-var logger = require('./3.logger');
+const logger = require('./3.logger');
 
-console.log(logger.endPoint);
+console.log(logger.endPoint); //logger exports endpoint and url.so we can catch them like this in here
 logger.log('hello');
+
+//if the export is module.export = log no need to access it with logger.log(), can dirrectly access as log()
